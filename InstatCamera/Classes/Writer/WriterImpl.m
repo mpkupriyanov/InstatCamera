bb849ba0683f7a29b98aa68043093b50024180b2
mpkupriyanov <mpkupriyanov@gmail.com>
2020-11-02 22:41:57 +0300

    fixed crash when append sample buffer

--- InstatCamera/Classes/Writer/WriterImpl.m
+++ InstatCamera/Classes/Writer/WriterImpl.m
@@ -146,16 +146,18 @@ static const NSTimeInterval kDefaultChunkDuration = 5.000f;
     
     CMTime presentationTimeStamp = CMSampleBufferGetPresentationTimeStamp(sampleBuffer);
     
-    if (mediaType == AVMediaTypeAudio) {
-        if (_audioAssetWriterInput.readyForMoreMediaData && _audioAssetWriterInput != nil) {
-            if (![_audioAssetWriterInput appendSampleBuffer: sampleBuffer]) {
-                NSLog(@"audio append says NO: %ld, %@", (long)_assetWriter.status, _assetWriter.error);
+    if (_assetWriter.status == AVAssetWriterStatusWriting) {
+        if (mediaType == AVMediaTypeAudio) {
+            if (_audioAssetWriterInput.readyForMoreMediaData && _audioAssetWriterInput != nil) {
+                if (![_audioAssetWriterInput appendSampleBuffer: sampleBuffer]) {
+                    NSLog(@"audio append says NO: %ld, %@", (long)_assetWriter.status, _assetWriter.error);
+                }
             }
-        }
-    } else {
-        if (_videoAssetWriterInput.readyForMoreMediaData && _videoAssetWriterInput != nil) {
-            if (![_videoAssetWriterInput appendSampleBuffer: sampleBuffer]) {
-                NSLog(@"video append says NO: %ld, %@", (long)_assetWriter.status, _assetWriter.error);
+        } else {
+            if (_videoAssetWriterInput.readyForMoreMediaData && _videoAssetWriterInput != nil) {
+                if (![_videoAssetWriterInput appendSampleBuffer: sampleBuffer]) {
+                    NSLog(@"video append says NO: %ld, %@", (long)_assetWriter.status, _assetWriter.error);
+                }
             }
         }
     }
